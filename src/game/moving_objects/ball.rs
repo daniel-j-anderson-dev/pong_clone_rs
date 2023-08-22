dd71e1bd7c9486c6cb9f4c857437884948d0a4b5
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-08-22 17:12:46 -0400

    Collisions and keep in window look at next frame.

--- src/game/moving_objects/ball.rs
+++ src/game/moving_objects/ball.rs
@@ -2,7 +2,7 @@ use crate::game::moving_objects::{MovingObject, paddle::Paddle, Rectangle};
 
 use macroquad::prelude::{
     Color,
-    Vec2,
+    DVec2,
     screen_width,
     screen_height,
     draw_rectangle,
@@ -10,11 +10,11 @@ use macroquad::prelude::{
 
 pub struct Ball {
     pub boundary: Rectangle,
-    pub velocity: Vec2,
+    pub velocity: DVec2,
     pub color: Color,
 }
 impl Ball {
-    pub fn new(boundary: Rectangle, velocity: Vec2, color: Color) -> Self {
+    pub fn new(boundary: Rectangle, velocity: DVec2, color: Color) -> Self {
         return Self {
             boundary,
             velocity,
@@ -25,7 +25,7 @@ impl Ball {
         if let Some(intersection) = self.boundary.get_intersection_with(&paddle.boundary) {
             let reflection_angle =
                 (self.boundary.center().x - intersection.center().x) / (intersection.w / 2.0).atan();
-            self.velocity = Vec2 {
+            self.velocity = DVec2 {
                 x: reflection_angle.cos() * self.velocity.length(),
                 y: reflection_angle.sin() * self.velocity.length(),
             };
@@ -34,29 +34,40 @@ impl Ball {
 }
 impl MovingObject for Ball {
     fn update_position(&mut self) {
+        self.keep_in_window();
         self.boundary.x += self.velocity.x;
         self.boundary.y += self.velocity.y;
     }
-    fn draw(&self) {
-        draw_rectangle(self.boundary.x, self.boundary.y, self.boundary.w, self.boundary.h, self.color);
-    }
     fn keep_in_window(&mut self) {
-        if self.boundary.x < 0.0 {
+        if self.boundary.x + self.velocity.x < 0.0 {
             self.velocity.x *= -1.0;
             // self.winner = Some(1);
         }
-        if self.boundary.x + self.boundary.w > screen_width() {
+        if self.boundary.x + self.boundary.w + self.velocity.x > screen_width() as f64{
             // self.winner = Some(0)
             self.velocity.x *= -1.0;
         }
-        if self.boundary.y < 0.0 {
+        if self.boundary.y + self.velocity.y < 0.0 {
             self.velocity.y *= -1.0;
         }
-        if self.boundary.y + self.boundary.h > screen_height() {
+        if self.boundary.y + self.boundary.h + self.velocity.y > screen_height() as f64{
             self.velocity.y *= -1.0;
         }
     }
-    fn handle_collision(&mut self, other: Box<dyn MovingObject>) {
-        
+    fn handle_collision<MovingObj: MovingObject>(&mut self, other: &mut MovingObj) {}
+    fn boundary(&self) -> &Rectangle {
+        return &self.boundary;
+    }
+    fn color(&self) -> &Color {
+        return &self.color;
+    }
+    fn velocity(&self) -> &DVec2 {
+        return &self.velocity;
+    }
+    fn invert_x_velocity(&mut self) {
+        self.velocity.x *= -1.0;
+    }
+    fn invert_y_velocity(&mut self) {
+        self.velocity.y *= -1.0;
     }
 }
