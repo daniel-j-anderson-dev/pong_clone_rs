965c2217c9eb1874a97ef2543746470a1fefb744
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-08-22 17:13:01 -0400

    Added a window configuration.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use macroquad::prelude::{
+    Conf, 
     KeyCode,
     is_key_down,
     next_frame,
@@ -6,7 +7,22 @@ use macroquad::prelude::{
 
 pub mod game;
 
-#[macroquad::main("pong_clone")]
+fn window_configuration() -> Conf {
+    return Conf {
+        window_title: "Pong clone".to_owned(),
+        fullscreen: false,
+        window_width: 800,
+        window_height: 600,
+        high_dpi: true,
+        sample_count: 1,
+        window_resizable: true,
+        // platform: ,
+        // icon: ,
+        ..Default::default()
+    };
+}
+
+#[macroquad::main(window_configuration())]
 async fn main() {
     let mut game_state = game::State::new();
     loop {
